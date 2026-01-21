d5b2d2d1184df93ec572c2006245662eb6bbf2af
Jonathan Swan <168398101+Jswan-2005@users.noreply.github.com>
2026-01-21 16:30:23 +1100

    Update CPU_Scheduler.py
    Added lottery scheduler & task switching outputs

--- CPU_Scheduler.py
+++ CPU_Scheduler.py
@@ -3,18 +3,19 @@ This project simulates multiple CPU Schedulers
 1. FIFOScheduler : FIFO (First in First Out) Scheduler
 2. STCFScheduler : STCF (Shortest Time to Complete First) Scheduler
 3. RRScheduler : RR (Round Robin) Scheduler
+4. Lottery Scheduler : Tickets based on total time to execute process
 
 Each scheduler dynamically prints the current process (if any) running at every second.
 At the end (when every process has completed executing) key metrics are printed for each process
-These metrics are : Arrival Time, Completion Time, Turnaround Time (Completion - Arrival), Response Time (When the process was first exectued by the CPU)
+These metrics are : Arrival Time, Completion Time, Turnaround Time (Completion - Arrival), Response Time (When the process was first executed by the CPU)
+
 '''
 
 import sys
 import copy
 import time as tm
 import os
-
-
+import random
 
 class Process:
     def __init__(self, arrival, time_to_run, pid):
@@ -31,11 +32,16 @@ def PrintProccesses(pl, scheduler):
     for p in pl:
         print(f"Process {p.pid} -> Arrival Time {p.t_arrival} -> Completion Time {p.t_completion}, Turnaround Time {p.t_turnaround}, Response Time {p.t_response}")
 
+def TaskSwitching(process_origin_pid,process_switch_pid,time):
+    print(f'Process {process_origin_pid} -> {process_switch_pid} at time {time} seconds')
+
 def FIFOScheduler(pl):
     scheduler_list = sorted(pl, key=lambda p: p.t_arrival)
     time = 0
     while scheduler_list:
         current_process = scheduler_list[0]
+        if time > 0:
+            TaskSwitching(temp.pid, current_process.pid, time)
         while current_process.t_arrival > time:
             DynamicPrint(None, time)
             time += 1
@@ -50,7 +56,7 @@ def FIFOScheduler(pl):
 
         current_process.t_completion = time
         current_process.t_turnaround = current_process.t_completion - current_process.t_arrival
-        scheduler_list.pop(0)
+        temp = scheduler_list.pop(0)
     print()
     PrintProccesses(pl, 'FIFOScheduler')
 
@@ -60,6 +66,7 @@ def STCFScheduler(pl):
     scheduler_list = sorted(pl, key=lambda p: p.t_arrival)
     scheduler_runner = []
     time = 0
+    temp = None
     while scheduler_list:
         time += 1
         for p in pl:
@@ -69,7 +76,11 @@ def STCFScheduler(pl):
                 pl.remove(p)
 
         if scheduler_runner:
+            if temp and temp.pid != scheduler_runner[0].pid:
+                TaskSwitching(temp.pid, scheduler_runner[0].pid, time)
+
             current_process = scheduler_runner[0]
+            temp = current_process
             DynamicPrint(current_process, time)
             current_process.t_time_to_run -= 1
 
@@ -92,9 +103,8 @@ def DynamicPrint(current_process,time):
         print(f"Current Process {current_process.pid} running at time {time}")
     else:
         print(f"No current processes at time {time}")
-    tm.sleep(1)
+    tm.sleep(0.5)
     os.system('cls')
-
 
 def RRScheduler(pl,timesplice):
     pl_copy = pl.copy()
@@ -132,10 +142,55 @@ def RRScheduler(pl,timesplice):
                     temp = scheduler_runner[0]
                     scheduler_runner.remove(scheduler_runner[0])
                     scheduler_runner.append(temp)
+                    if temp.pid != scheduler_runner[0].pid:
+                        TaskSwitching(temp.pid,scheduler_runner[0].pid,time)
+
         else:
             DynamicPrint(None, time)
     print()
     PrintProccesses(pl_copy, 'RRScheduler')
+
+def LotteryScheduler(pl):
+    pl_copy = pl.copy()
+    time = 0
+    scheduler_list = sorted(pl, key=lambda p: p.t_arrival)
+    scheduler_runner = []
+    lottery_sum = []
+    process_list = []
+    random_ticket = None
+    process_ran = False
+    while scheduler_list:
+        time += 1
+        current_process = None
+
+        for p in pl:
+            if p.t_arrival <= time and p not in scheduler_runner:
+                lottery_sum += [p.pid for _ in range(p.t_time_to_run)]
+                scheduler_runner.append(p)
+
+        if lottery_sum:
+            random_ticket = random.choice(lottery_sum)
+            for process in scheduler_runner:
+                if random_ticket == process.pid:
+                    if process_list and process_list[-1] != random_ticket:
+                        TaskSwitching(process_list[-1], random_ticket,time)
+                    current_process = process
+                    process_list.append(process.pid)
+                    if process.t_response is None:
+                        process.t_response = time - process.t_arrival
+
+        if current_process:
+            current_process.t_time_to_run -= 1
+            lottery_sum.remove(current_process.pid)
+            if current_process.t_time_to_run == 0:
+                current_process.t_completion = time+1
+                current_process.t_turnaround = current_process.t_completion - current_process.t_arrival
+                scheduler_list.remove(current_process)
+                scheduler_runner.remove(current_process)
+        DynamicPrint(current_process, time)
+    print()
+    PrintProccesses(pl_copy, 'LotteryScheduler')
+
 
 def main():
     '''  Process(arrival, time_to_run, pid)
@@ -145,12 +200,11 @@ def main():
     '''
 
     #Example run-through with RR Scheduler with a timesplice of 3
-    p1 = Process(5, 10, 1)
-    p2 = Process(7, 5, 2)
-    p3 = Process(15, 3,3)
+    p1 = Process(5, 3, 1)
+    p2 = Process(5,10,2)
 
-    process_list = [p1, p2, p3]
-    RRScheduler(process_list,3)
+    process_list = [p1,p2]
+    LotteryScheduler(process_list)
 
 if __name__ == '__main__':
     main()
